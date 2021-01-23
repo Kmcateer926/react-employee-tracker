cc45aab94b19a6155f8ee91f0fefb0e90e38089f
Kevin McAteer <kmcateer926@gmail.com>
2021-01-23 08:50:04 -0500

    api results are showing in the terminal, now need to display results on page

--- src/componenets/EmployeeResults.jsx
+++ src/componenets/EmployeeResults.jsx
@@ -43,12 +43,45 @@ class EmployeeResults extends Component {
       .catch((err) => {
         console.log(err);
       });
-
-      
-    
-
-
   };
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-12">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">
+                    <h2>Image</h2>
+                  </th>
+                  <th scope="col">
+                    <h2>First Name</h2>
+                  </th>
+                  <th scope="col">
+                    <h2>Last Name</h2>
+                  </th>
+                  <th scope="col">
+                    <h2>Email</h2>
+                  </th>
+                  <th scope="col">
+                    <h2>Age</h2>
+                  </th>
+                  <th scope="col">
+                    <h2>Gender</h2>
+                  </th>
+                  <th scope="col">
+                    <h2>Country</h2>
+                  </th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default EmployeeResults;
